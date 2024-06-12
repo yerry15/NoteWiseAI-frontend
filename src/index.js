e59542fa86f3db65ca3fd017ee9ad7a66c659369
Jerry Feng <jerryfeng@Jerrys-Laptop.local>
2024-06-11 22:05:00 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function HelloWorld(){
+  const[message, setMessage] = useState('Hello, World');
+  const handleClick = async() => {
+    try{
+      const response = await fetch("http://127.0.0.1:8000/hello")
+      const data = await response.json();
+      console.log('Fetched message:', data.message);
+      setMessage(data.message);
+    } catch (error) {
+      console.error('Error fetching the message', error);
+    }
+
+  };
+
+  return (
+  
+    <div className="test">
+      <button className="greeting" onClick = {handleClick}>Click me</button>
+      <p>
+      {message}
+      
+      </p>
+
+    </div>
+
+  
+  )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
 
